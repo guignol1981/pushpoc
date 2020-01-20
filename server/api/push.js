f25f7107685c3c145e7e34a02e455f04224f94ad
CSDTI <csdti@iMac-de-CSDTI.local>
2020-01-20 11:09:54 -0500

    * fix php script path

--- server/api/push.js
+++ server/api/push.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', (req, res) => {
     var exec = require("child_process").exec;
 
-    exec("php createPushPackage.php", function
+    exec("php " + path.join(__dirname, 'createPushPackage.php'), function
         (error, stdout, stderr) {
-        console.log(error);
+
+        console.debug(error);
+        console.debug(stdout);
+
         res.send(stdout);
     });
 });
